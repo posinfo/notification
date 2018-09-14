3788fa67f9b422df391a332d8d819ffcbd947b33
posinfo <paulooliveiradesouza@hotmail.com>
2018-09-14 13:19:25 -0300

    Commit Initial

--- _app/Email.php
+++ _app/Email.php
@@ -18,21 +18,21 @@ class Email {
     
     private $mail = \stdClass::class;
     
-    public function __construct() {
+    public function __construct( $smtpDebug, $host, $smtpAut, $username, $password, $smtpSecure, $port, $emailFrom, $nameFrom) {
         $this->mail = new PHPMailer(true); 
         
-        $this->mail->SMTPDebug = 0;                                 // Enable verbose debug output
+        $this->mail->SMTPDebug = $smtpDebug;                                 // Enable verbose debug output
         $this->mail->isSMTP();                                      // Set mailer to use SMTP
-        $this->mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
-        $this->mail->SMTPAuth = true;                               // Enable SMTP authentication
-        $this->mail->Username = 'posinfobr@gmail.com';                 // SMTP username
-        $this->mail->Password = 'webmaster2008';                           // SMTP password
-        $this->mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
-        $this->mail->Port = 587;                                    // TCP port to connect to
+        $this->mail->Host = $host;  //'smtp.gmail.com' Specify main and backup SMTP servers
+        $this->mail->SMTPAuth = $smtpAut;                               // true Enable SMTP authentication
+        $this->mail->Username = $username;                 // 'posinfobr@gmail.com'SMTP username
+        $this->mail->Password = $password;                           // 'webm8'SMTP password
+        $this->mail->SMTPSecure = $smtpSecure;                            // 'tls'Enable TLS encryption, `ssl` also accepted
+        $this->mail->Port = $port;                                    // 587 TCP port to connect to
         $this->mail->CharSet = 'utf-8';                                    // TCP port to connect to
         $this->mail->setLanguage('br');                                    // TCP port to connect to
         $this->mail->isHTML(true);                                    // TCP port to connect to
-        $this->mail->setFrom('paulooliveiradesouza@hotmail.com', 'Paulo Oliveira');
+        $this->mail->setFrom($emailFrom, $nameFrom); // 'paulooliveiradesouza@hotmail.com' 'Paulo Oliveira' 
     }
 
 
--- test/index.php
+++ test/index.php
@@ -1,9 +1,9 @@
 <?php
-require __dir__. "/vendor/autoload.php";
+require __dir__. "/../vendor/autoload.php";
 use Notification\Email;
 
 
-$newemail = new Email();
+$newemail = new Email(2, 'smtp.gmail.com', true, 'posinfobr@gmail.com', 'webmaster2008', 'tls', 587, 'paulooliveiradesouza@hotmail.com', 'Paulo Oliveira' );
 $emailSent = $newemail->sendMail("Funcional", "<p>Ione Paulo é  <strong>Gostoso....</strong></p>", "ionepos@hotmail.com", "Ione Oliveira", "paulooliveiradesouza@hotmail.com", "Paulo");
 if($emailSent):
     echo 'Pos E-mail Enviado com sucesso, e uma alteração feita.';  
